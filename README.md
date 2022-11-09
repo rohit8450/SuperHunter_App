00fb295cc6c0075904d3701ae1da56f25c5783f3
rohit8450 <rohitrajthakur10@gmail.com>
2022-11-09 22:44:30 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+﻿# SuperHero Hunter- Mohit Goyal- Coding Ninjas <br>
+## Links: <br>
+Online Working Project - https://superhero.mohitgoyal.me/<br>
+Video Explanation Link  - https://youtu.be/hmVysWDTqz0 <br>
+GitHub Repo Link - https://github.com/rohit8450/superhero-hunter-using-marvel-api <br>
+
+
+## Screenshots <br>
+## Preview - Home <br>
+  <img src="/images/home.png" alt="Home Screenshot" srcset=""> <br>
+## Preview - Favourite <br>
+  <img src="images/favourite.png" alt="Stopwatch Screenshot" srcset=""> <br>
+## Preview - About Hero <br>
+  <img src="images/about.png?raw=true" alt="Stopwatch Screenshot" srcset=""> <br>
+
+## Video Preview <br>
+[![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/hmVysWDTqz0/0.jpg)](https://www.youtube.com/watch?v=hmVysWDTqz0)
+
+
+#### Thank you!
+
+
---
+++ js/about.js
@@ -0,0 +1,35 @@
+// Get Items that are stored in Local Storage 
+//  I'll Get Items through ID (Bacause all have different Id's)
+var resultId = localStorage.getItem('id');
+// Fetch Will be done!
+fetch();
+// Get all the data from Fetch 
+function fetch(){
+    var Request = new XMLHttpRequest();
+    // By Using Result Id, I will fetch agian with API 
+    var url = `https://gateway.marvel.com/v1/public/characters/${resultId}?apikey=b309871f10806da0e2b3918da9a73af5&hash=c8ee13381c07c4d76e309375612300a5&ts=1`;
+    Request.open('get',url,true);
+    Request.send();
+    Request.onload = function(){
+        var response = JSON.parse(Request.response);
+        // Print all the Resoponses in the formate of JSON
+        console.log(response);
+        // According to all Id's i will get elemets and change its inner HTML by the Responses
+        document.getElementById("img-cont").innerHTML = `<img src=${response.data.results[0].thumbnail.path
+        }/portrait_xlarge.jpg class="card-img-top" alt="..." id="marvel-img">`;
+            document.getElementById("name").innerHTML = '<b>Name: </b> ' + response.data.results[0].name;
+            document.getElementById("id").innerHTML = '<b>Hero ID: </b> ' + response.data.results[0].id ;
+            document.getElementById("desc").innerHTML = '<b>Description: </b> ' + response.data.results[0].description ;
+            document.getElementById("comic").innerHTML = '<b>Comic Available: </b>'+ response.data.results[0].comics.available ;
+            document.getElementById("series").innerHTML = '<b>Series Available: </b>'+ response.data.results[0].series.available ;
+            document.getElementById("stories").innerHTML = '<b>Stories Available: </b>'+ response.data.results[0].stories.available ;
+            document.getElementById("count").innerHTML = '<b>Count: </b>'+ response.data.count ;
+            document.getElementById("modified").innerHTML = '<b>Modified: </b>'+ response.data.results[0].modified;
+            document.getElementById("status").innerHTML = '<b>Status: </b>'+ response.status;
+            document.getElementById("total").innerHTML = '<b>Total: </b>'+ response.data.total;
+            document.getElementById("limit").innerHTML = '<b>Limit: </b>'+ response.data.limit;
+            document.getElementById("offset").innerHTML = '<b>Offset: </b>'+ response.data.offset;
+            document.getElementById("code").innerHTML = '<b>Code: </b>'+ response.code;
+            
+    }
+}
